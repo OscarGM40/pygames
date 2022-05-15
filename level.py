e633fd2a06aeadf1afc62b55224d210642f1abe4
Oscar <oscargm28@hotmail.com>
2022-05-15 19:11:42 +0200

    PYGAME-02] added sprites to movements

--- level.py
+++ level.py
@@ -54,7 +54,8 @@ class Level:
     # self.visible_sprites.draw(self.display_surface)
     self.visible_sprites.custom_draw(self.player)
     self.visible_sprites.update()
-    debug(self.player.direction)
+    # debug(self.player.direction)
+    debug(self.player.status)
 
 class YSortCameraGroup(pygame.sprite.Group):  # extiende de Group
 
