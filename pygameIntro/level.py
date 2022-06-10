7b35b0f9f9527c4b8fa65daeaf2a31be8057611f
Oscar <oscargm28@hotmail.com>
2022-06-10 19:42:56 +0200

    pygame is God

--- pygameIntro/level.py
+++ pygameIntro/level.py
@@ -1,6 +1,12 @@
 import pygame
+from random import randint
 
 class Level():
+  start_time = 0
+  snail_frame_index = 0
+  snail_frames = []
+  fly_frame_index = 0
+  fly_frames = []
   def __init__(self):
     # SUPERFICES: creo una regular surface con pygame.Surface((width, height))
     # test_surface = pygame.Surface((100,200))
@@ -10,8 +16,18 @@ class Level():
     self.create_player()
     self.create_enemy()
     self.create_UI()
+    self.display_score()
     self.player_gravity = 0
     self.player_is_jumping = False
+    self.enemies_list = []
+    self.enemies_surf = []
+    
+    self.types = []
+    self.enemies = {
+      'rects': self.enemies_list,
+      'surfs': self.enemies_surf,
+      'types': self.types
+    }
 
   def create_map(self):
     # IMAGENES: para importar una imagen se usa pygame.image.load(ruta):
@@ -19,13 +35,53 @@ class Level():
     self.ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
     
   def create_player(self):
-    self.player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+    player_walk_1 = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+    player_walk_2 = pygame.image.load('graphics/Player/player_walk_2.png').convert_alpha()
+    self.player_walk = [player_walk_1, player_walk_2]
+    self.player_index = 0
+    self.player_jump = pygame.image.load('graphics/Player/jump.png').convert_alpha()
+    self.player_surf = self.player_walk[self.player_index]
     self.player_rect = self.player_surf.get_rect(midbottom = (80,300)) 
+
+
+  
+  def create_standing_player(self):
+    self.player_standing_surf = pygame.image.load('graphics/Player/player_stand.png').convert_alpha()
+    # self.player_standing_surf = pygame.transform.scale(self.player_standing_surf,(150,150))
+    self.player_standing_surf = pygame.transform.rotozoom(self.player_standing_surf,0,2)
+    self.player_standing_rect = self.player_standing_surf.get_rect(center = (400,200))
+    return (self.player_standing_surf, self.player_standing_rect)
+  
+  def create_game_over_UI(self):
+    self.text_font = pygame.font.Font('font/Pixeltype.ttf',50) 
+    self.game_title_surf = self.text_font.render("Pixel Runner",False,(111,196,169))
+    self.game_title_rect = self.game_title_surf.get_rect(center = (410,80))
+    self.game_message = self.text_font.render("Press space to run",False,(111,196,169))
+    self.game_message_rect = self.game_message.get_rect(center = (410,340))
+    return(self.game_title_surf, self.game_title_rect, self.game_message, self.game_message_rect)
     
+  
   def create_enemy(self):
-    self.snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-    self.snail_rect = self.snail_surf.get_rect(bottomright = (600,300))
-    
+    condition = randint(0,2)
+    if condition == 0:
+      self.snail_frame_1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+      self.snail_frame_2 = pygame.image.load('graphics/snail/snail2.png').convert_alpha()
+      self.snail_frames = [self.snail_frame_1, self.snail_frame_2]
+      self.snail_frame_index = 0
+      self.snail_surf = self.snail_frames[0]
+      self.snail_rect = self.snail_surf.get_rect(bottomright = (randint(900,1100),300))
+      self.type = "snail"
+      return (self.snail_surf,self.snail_rect, self.type)
+    else:
+      self.fly_frame_1 = pygame.image.load('graphics/Fly/Fly1.png').convert_alpha()
+      self.fly_frame_2 = pygame.image.load('graphics/Fly/Fly2.png').convert_alpha()
+      self.fly_frames = [self.fly_frame_1, self.fly_frame_2]
+      self.fly_frame_index = 0
+      self.fly_surf = self.fly_frames[0]
+      self.fly_rect = self.fly_surf.get_rect(bottomright = (randint(900,1100),200))
+      self.type = "fly"
+      return (self.fly_surf,self.fly_rect,self.type)
+  
   def create_UI(self):
     # TEXTOS: hay que crear una fuente con la clase pygame.font.Font(family,size as Integer)
     # text_font = pygame.font.Font(None,50) # None será la fuente por defecto
@@ -34,7 +90,14 @@ class Level():
     self.lives_font = pygame.font.Font('font/Pixeltype.ttf',40) 
     self.lives_surf = self.lives_font.render("Lives:",False,pygame.Color('Black'))
     self.lives_rect = self.lives_surf.get_rect(topleft = (10,10))
-    self.score_surf = self.text_font.render("Score",False,(64,64,64))
-    self.score_rect = self.score_surf.get_rect(center = (400,50))
+    # self.score_surf = self.text_font.render("Score",False,(64,64,64))
+    # self.score_rect = self.score_surf.get_rect(center = (400,50))
 
-    
+  def display_score(self):
+    # global current_time <- forma uno de hacer una variable global
+    current_time = int(pygame.time.get_ticks() / 1000) - self.start_time
+    self.score_surf = self.text_font.render(f'Score {current_time}',False,(64,64,64))    
+    self.score_rect = self.score_surf.get_rect(center = (400,50))
+    return current_time 
+  
+        
--- pygameIntro/main.py
+++ pygameIntro/main.py
@@ -14,7 +14,48 @@ class Game:
     self.clock = pygame.time.Clock()
     self.game_active = True
     self.level = Level()
+    self.score = 0
+    self.text_font = pygame.font.Font('font/Pixeltype.ttf',50) 
+    # siempre hay que sumar 1 más a USERVENT por cada timer que cree 
+    self.obstacle_timer = pygame.USEREVENT  + 1
+    # pygame.time.set_timer(functionToExecute, milliseconds)
+    pygame.time.set_timer(self.obstacle_timer, 1300)
+    # más timers para animar los enemies
+    self.snail_animation_timer = pygame.USEREVENT + 2
+    pygame.time.set_timer(self.snail_animation_timer, 300)
+    self.fly_animation_timer = pygame.USEREVENT + 3
+    pygame.time.set_timer(self.fly_animation_timer, 200)
 
+  def collisions(self,player,obstacles):
+    if obstacles:
+      for obstacle_rect in obstacles:
+        if player.colliderect(obstacle_rect):
+          return False
+    return True
+
+  def player_animation(self):
+    global player_surf,player_index
+    # si no toca el suelo cambio el sprite
+    if self.level.player_rect.bottom < 300:
+      self.level.player_surf = self.level.player_jump
+    else:
+      self.level.player_index += 0.1
+      self.level.player_surf = self.level.player_walk[int(self.level.player_index) % len(self.level.player_walk)]
+    
+  def create_enemies(self):
+    # for enemy_rect in self.level.enemies_list:
+    if(self.level.enemies):
+      for enemy_rect in self.level.enemies['rects']:
+        enemy_rect.x -= 5
+        if enemy_rect.bottom == 300:
+          self.screen.blit(self.level.snail_surf, enemy_rect)
+        else:
+          self.screen.blit(self.level.fly_surf, enemy_rect)
+          # list comprehension(x for x in list if ...):  
+      self.level.enemies_list = [enemy_rect for enemy_rect in self.level.enemies_list if enemy_rect.x > -100]
+      
+    return self.level.enemies_list
+  
 # sin embargo falta mantener el script indefinidamente
   def run(self):
     while True:
@@ -40,6 +81,39 @@ class Game:
               self.level.player_gravity = 0
               self.level.player_rect.bottom = 300
               self.level.snail_rect.right = 800
+              self.level.start_time = int(pygame.time.get_ticks() / 1000)
+        if event.type == self.obstacle_timer and self.game_active:
+          newEnemy = self.level.create_enemy()
+          self.level.enemies['surfs'].append(newEnemy[0])
+          self.level.enemies['rects'].append(newEnemy[1])
+          self.level.enemies['types'].append(newEnemy[2])
+          self.level.enemies_surf.append(newEnemy[0])
+          self.level.enemies_list.append(newEnemy[1])
+          self.level.types.append(newEnemy[2])
+        if event.type == self.snail_animation_timer and self.game_active:
+          snail_frame_1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+          snail_frame_2 = pygame.image.load('graphics/snail/snail2.png').convert_alpha()
+          snail_frames = [snail_frame_1, snail_frame_2]
+          self.level.snail_surf = snail_frames[0]
+          if self.level.snail_frame_index == 0:
+            self.level.snail_frame_index = 1
+          else:
+            self.level.snail_frame_index = 0
+          self.level.snail_surf = snail_frames[self.level.snail_frame_index]
+        if event.type == self.fly_animation_timer and self.game_active:
+          fly_frame_1 = pygame.image.load('graphics/Fly/Fly1.png').convert_alpha()
+          fly_frame_2 = pygame.image.load('graphics/Fly/Fly2.png').convert_alpha()
+          fly_frames = [fly_frame_1,fly_frame_2]
+          self.level.fly_surf = fly_frames[0]
+          if self.level.fly_frame_index == 0:
+            self.level.fly_frame_index = 1
+          else:
+            self.level.fly_frame_index = 0
+          self.level.fly_surf = fly_frames[self.level.fly_frame_index]
+  
+                   
+          
+              
       if self.game_active:
         self.screen.blit(self.level.sky_surface,(0,0))
         self.screen.blit(self.level.ground_surface,(0,300))
@@ -47,13 +121,15 @@ class Game:
         pygame.draw.rect(self.screen,'#c0e8ec',self.level.score_rect,6)
         # pygame.draw.line(self.screen,'Pink',(0,0),(800,400),6)
         pygame.draw.ellipse(self.screen,(255,222,89),pygame.Rect(50,60,70,70))
+        self.score = self.level.display_score()
+        self.level.enemies_list = self.create_enemies()
+        # collisions
+        self.game_active = self.collisions(self.level.player_rect,self.level.enemies_list)
+        self.player_animation()
         self.screen.blit(self.level.score_surf,self.level.score_rect)
         self.screen.blit(self.level.lives_surf,self.level.lives_rect)
     
-        self.screen.blit(self.level.snail_surf,self.level.snail_rect)
-        self.level.snail_rect.x -= 3
-        if self.level.snail_rect.right <= 0: 
-          self.level.snail_rect.left = 800
+        # self.screen.blit(self.level.snail_surf,self.level.snail_rect)
     
         self.level.player_gravity += 1
         self.level.player_rect.bottom += self.level.player_gravity
@@ -62,9 +138,10 @@ class Game:
           self.level.player_rect.bottom = 300
         self.screen.blit(self.level.player_surf,self.level.player_rect)
 
+        
         # si toca el snail se acaba el juego
-        if self.level.player_rect.colliderect(self.level.snail_rect):
-          self.game_active = False
+        # if self.level.player_rect.colliderect(self.level.snail_rect):
+        #   self.game_active = False
   
         # pygame.key.get_pressed() devuelve un array de 0 y 1 con todas las teclas
         keys = pygame.key.get_pressed() # lo mejor es usarlo como un diccionario
@@ -73,7 +150,20 @@ class Game:
             self.level.player_is_jumping = True
             self.level.player_gravity = -20
       else:
-        self.screen.fill('yellow')
+        self.screen.fill((94,129,162))
+        self.level.enemies_list.clear() 
+        self.level.player_rect.midbottom = (80,300)
+        self.level.player_gravity = 0
+        self.screen.blit(self.level.create_standing_player()[0], self.level.create_standing_player()[1])
+        score_message = self.text_font.render(f'Your score: {self.score}', False, (111,196,169))
+        score_message_rect = score_message.get_rect(center = (400,330))
+        self.screen.blit(self.level.create_game_over_UI()[0], self.level.create_game_over_UI()[1])
+        
+        if self.score == 0:
+          self.screen.blit(self.level.create_game_over_UI()[2], self.level.create_game_over_UI()[3])
+        else:
+          self.screen.blit(score_message, score_message_rect)
+
       # en este bucle hay que llamar a display.update para que actualice todo
       pygame.display.update()
       self.clock.tick(60) # con la instancia de Clock fijo el framerrate máximo,y este bucle while True se ejecutará máximo 60 veces por segundo
